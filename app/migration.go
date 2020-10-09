db59e5f38f664e5f7a984a2a2d94b82b2f271713
jb terry lin <47049114+jbterrylin@users.noreply.github.com>
2020-10-09 15:15:24 +0800

    insert seed to user and area

--- app/migration.go
+++ app/migration.go
@@ -2,9 +2,11 @@ package app
 
 import (
 	"ekans/app/model"
+	"time"
 
 	"github.com/go-pg/pg/v10"
 	"github.com/go-pg/pg/v10/orm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // RegisterModels :
@@ -26,5 +28,29 @@ func RegisterModels(db *pg.DB) error {
 			return err
 		}
 	}
+
+	count, _ := db.Model((*model.User)(nil)).Count()
+	if count > 0 {
+		user := new(model.User)
+		user.Name = "Wang Lin Lee"
+		hashPassword, _ := bcrypt.GenerateFromPassword([]byte("1"), bcrypt.MinCost)
+		encodePW := string(hashPassword)
+		user.Password = encodePW
+		user.PhoneNumber = "60167560969"
+		user.Job = "Admin"
+		user.RegisterDateTime = time.Now()
+		db.Model(user).Insert()
+	}
+
+	count1, _ := db.Model((*model.Area)(nil)).Count()
+	if count1 == 0 {
+		area := new(model.Area)
+		area.AreaName = "Heart"
+		db.Model(area).Insert()
+		area1 := new(model.Area)
+		area1.AreaName = "Bone"
+		db.Model(area1).Insert()
+	}
+
 	return nil
 }
